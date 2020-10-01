f5f6c7d79198ed3b630f6166818db7fa2a63b32b
igorek1955 <igorek1955@gmaii.com>
2020-10-01 21:22:19 +0300

    adding owners list and updated controller . closes #8

--- pet-clinic-web/src/main/java/ru/springboot/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/springboot/petclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package ru.springboot.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.springboot.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/owners/index", "/owners/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
