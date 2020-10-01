48ccc9aadc8aa83d9cf303253a98383729031009
igorek1955 <igorek1955@gmaii.com>
2020-10-01 22:30:03 +0300

    refactored Map Impl to abstract ID generation. closes #10

--- pet-clinic-web/src/main/java/ru/springboot/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/springboot/petclinic/controllers/VetController.java
@@ -15,8 +15,8 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/index", "index.html"})
-    public String listVets(Model model){
+    @RequestMapping({"", "/", "/index", "index.html"})
+    public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
